96edb4e27ff83ac071b3c520b70db4263135f03e
kenza-20 <kenza.amiria@esprit.tn>
2025-02-20 12:30:38 +0100

    Initial commit

---
+++ src/main/java/com/example/gestioonrhetpaie/GestioonRhEtPaieApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioonrhetpaie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioonRhEtPaieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioonRhEtPaieApplication.class, args);
+	}
+
+}
