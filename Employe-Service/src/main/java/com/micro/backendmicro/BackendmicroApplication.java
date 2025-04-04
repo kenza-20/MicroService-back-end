f3f8b1da08caa45c6a61d166972cebe3fc243fb8
Maram Trabelsi <maramtrabelsi75@gmail.com>
2025-04-04 21:04:47 +0100

    docker images + docker-compose.yml **

--- Employe-Service/src/main/java/com/micro/backendmicro/BackendmicroApplication.java
+++ Employe-Service/src/main/java/com/micro/backendmicro/BackendmicroApplication.java
@@ -3,9 +3,11 @@ package com.micro.backendmicro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class BackendmicroApplication {
 
 	public static void main(String[] args) {
--- Recrutement/src/main/java/com/example/webdist/WebDistApplication.java
+++ Recrutement/src/main/java/com/example/webdist/WebDistApplication.java
@@ -2,8 +2,10 @@ package com.example.webdist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebDistApplication {
 
     public static void main(String[] args) {
