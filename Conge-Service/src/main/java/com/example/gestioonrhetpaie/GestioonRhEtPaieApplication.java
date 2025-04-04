2ea889800c409e5fb6f2721219e9f3770519a729
Maram Trabelsi <maramtrabelsi75@gmail.com>
2025-04-04 18:27:37 +0100

    microservices + dockerfiles

--- Conge-Service/src/main/java/com/example/gestioonrhetpaie/GestioonRhEtPaieApplication.java
+++ Conge-Service/src/main/java/com/example/gestioonrhetpaie/GestioonRhEtPaieApplication.java
@@ -3,9 +3,12 @@ package com.example.gestioonrhetpaie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GestioonRhEtPaieApplication {
 
 	public static void main(String[] args) {
